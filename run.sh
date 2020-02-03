7facd50c8cf6f69e569383328e119bc7d2f38567
root <admin@example.com>
2020-02-04 00:43:36 +0800

    update

--- run.sh
+++ run.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 #常用命令
 cd  /app;
+[ -f /app/chfs ]|| {
 cat /proc/cpuinfo | grep "model name"| egrep  -i  arm && unzip chfs-linux-arm64*
 cat /proc/cpuinfo | grep "model name"| egrep  -i  arm || unzip chfs-linux-amd64*
+}
+_conf  () {
 cat  > /config/chfs.ini  <<  'EOF'
 port=8080
 rule=admin:123456:RWD
@@ -14,22 +17,13 @@ html.title=CHFSHARE
 image.preview=true
 session.timeout=3600
 EOF
+}
+[  -f /config/chfs.ini] || _conf
 chmod +x /app/chfs
 /app/chfs --file=/config/chfs.ini
 
 
 #
-# 创建用户：
-# docker exec chfs addusr 用户名 密码 权限
-# 复制代码
-# 实例：
-# docker exec chfs addusr admin 123456 RWD
-# 复制代码
-# 删除用户：
-# docker exec chfs delusr 用户名
-# 复制代码
-# 实例：
-# docker exec chfs delusr admin
-# 复制代码
+
 #重启容器生效
 # docker  restart  chfs
