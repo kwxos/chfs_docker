3d3c9a24f1538ecad752fc682e13cf568bcc055b
静雨·安婵 <102129419+kwxos@users.noreply.github.com>
2025-02-11 21:38:14 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,30 @@
 #!/bin/bash
 #常用命令
 cd  /app;
+if [ ! -f /app/chfs ]; then
+    # 获取系统架构
+    ARCH=$(uname -m)
+    
+    # 根据系统架构选择合适的下载链接
+    if [ "$ARCH" == "x86_64" ]; then
+        # 64位系统
+        DOWNLOAD_URL="http://iscute.cn/tar/chfs/3.1/chfs-linux-x86_64-3.1.zip"
+    elif [ "$ARCH" == "aarch64" ]; then
+        # ARM64架构系统
+        DOWNLOAD_URL="http://iscute.cn/tar/chfs/3.1/chfs-linux-arm64-3.1.zip"
+    elif [ "$ARCH" == "armv7l" ]; then
+        # ARM32架构系统
+        DOWNLOAD_URL="http://iscute.cn/tar/chfs/3.1/chfs-linux-arm32-3.1.zip"
+    else
+        echo "Unsupported architecture: $ARCH"
+        exit 1
+    fi
+    # 下载并解压对应的压缩包
+    wget $DOWNLOAD_URL
+    unzip $(basename $DOWNLOAD_URL)
+    
+    echo "解压完成，安装已完成"
+fi
 _conf  () {
 cat  > /config/chfs.ini  <<  'EOF'
 port=8080
